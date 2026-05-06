2d6efd9fe6ede0b4481049076fbc38a0a8df7c36
kypicl <kyclarke@gmail.com>
2026-05-05 17:15:28 -0700

    aboutus committee minor changes

--- src/app/(frontend)/aboutus/page.tsx
+++ src/app/(frontend)/aboutus/page.tsx
@@ -48,15 +48,19 @@ export default async function AboutUsPage() {
       <div className="flex justify-center">
       <div className="h-[180px] w-[180px] md:h-[150px] md:w-[150px] lg:h-[180px] lg:w-[180px]">
        {terriphoto?.url && (
-        <Media imgClassName="h-full w-full object-cover rounded-full" resource={terriphoto} />
+        <Image
+        src={terriphoto.url}
+        alt={terriphoto.alt || ""}
+        className="h-full w-full object-cover rounded-full"
+        />
        )}
       </div>
       </div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Terri Abelar</div>
           <div className="text-md pb-2.5">Business Officer</div>
-          <div className="text-sm">Aging Solutions, Inc.</div>
-          <div className="text-sm underline"><Link href="https://agingsolutions.com/">agingsolutions.com</Link></div>
-          <div className="text-sm">(415) 324-5088</div>
+          <div className="text-sm break-all px-2.5">Aging Solutions, Inc.</div>
+          <div className="text-sm break-all px-2.5 underline"><Link href="https://agingsolutions.com/">agingsolutions.com</Link></div>
+          <div className="text-sm break-all px-2.5">(415) 324-5088</div>
         </div>
 
             <div className="mb-5 md:mb-0 text-center shadow lg:shadow md:shadow-none rounded-lg rounded-lg w-[250px] lg:w-[250px] md:w-[170px] py-5 place-items-center place-self-center">
@@ -75,10 +79,10 @@ export default async function AboutUsPage() {
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Lorena Garbarino</div>
           <div className="text-md px-2">5-Minute Company Highlight/Gift</div>
           <div className="text-md pb-2.5">Sponsor Coordinator</div>
-          <div className="text-sm">Suncrest Hospice</div>
-          <div className="text-sm underline"><Link href="https://www.suncresthospicecare.com/">suncresthospicecare.com/</Link></div>
-          <div className="text-sm">LGarbarino@suncresthospicecare.com</div>
-          <div className="text-sm">(415) 949-8354</div>
+          <div className="text-sm break-all px-2.5">Suncrest Hospice</div>
+          <div className="text-sm break-all px-2.5 underline"><Link href="https://www.suncresthospicecare.com/">suncresthospicecare.com/</Link></div>
+          <div className="text-sm break-all px-2.5">(415) 949-8354</div>
+          <div className="text-sm break-all px-2.5">LGarbarino@suncresthospicecare.com</div>
         </div>
 
             <div className="h-full text-center shadow lg:shadow md:shadow-none rounded-lg rounded-lg w-[250px] lg:w-[250px] md:w-[170px] py-5 place-items-center place-self-center">
@@ -95,9 +99,9 @@ export default async function AboutUsPage() {
       </div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">William Foster</div>
           <div className="text-md pb-2.5 lg:px-2">Director of Community Relations</div>
-          <div className="text-sm">The Redwoods, A Community of Seniors</div>
-          <div className="text-sm underline"><Link href="https://www.theredwoods.org/">theredwoods.org</Link></div>
-          <div className="text-sm">wfoster@theredwoods.org</div>
+          <div className="text-sm break-all px-2.5">The Redwoods, A Community of Seniors</div>
+          <div className="text-sm break-all px-2.5 underline"><Link href="https://www.theredwoods.org/">theredwoods.org</Link></div>
+          <div className="text-sm break-all px-2.5">wfoster@theredwoods.org</div>
         </div>
       </div>
       </div>
@@ -119,9 +123,9 @@ export default async function AboutUsPage() {
       </div></div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Johnell Davidson</div>
           <div className="text-md pb-2.5">Speaker Seeker</div>
-          <div className="text-sm ">Neighborly Senior Placement</div>
-          <div className="text-sm">Johnell@neighborlyseniorplacement.com</div>
-          <div className="text-sm">(415) 294-0505</div>
+          <div className="text-sm break-all px-2.5 ">Neighborly Senior Placement</div>
+          <div className="text-sm break-all px-2.5">(415) 294-0505</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">johnell@neighborlyseniorplacement.com</div>
         </div>
 
             <div className="mb-5 md:mb-0 h-full text-center shadow lg:shadow md:shadow-none rounded-lg rounded-lg w-[250px] lg:w-[250px] md:w-[170px] py-5 place-items-center place-self-center">
@@ -137,10 +141,10 @@ export default async function AboutUsPage() {
       </div></div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Libby Wood</div>
           <div className="text-md pb-2.5">Certified Senior Move Manager</div>
-          <div className="text-sm">Senior Settlers</div>
-          <div className="text-sm underline"><Link href="https://www.senior-settlers.com/">senior-settlers.com</Link></div>
-          <div className="text-sm">(415) 755-3340</div>
-          <div className="text-sm">libby@senior-settlers.com</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">Senior Settlers</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5 underline"><Link href="https://www.senior-settlers.com/">senior-settlers.com</Link></div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">(415) 755-3340</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">libby@senior-settlers.com</div>
         </div>
 
             <div className="mb-5 md:mb-0 h-full text-center shadow lg:shadow md:shadow-none rounded-lg rounded-lg w-[250px] lg:w-[250px] md:w-[170px] py-5 place-items-center place-self-center">
@@ -156,10 +160,10 @@ export default async function AboutUsPage() {
       </div></div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Carly Migdal</div>
           <div className="text-md pb-2.5"></div>
-          <div className="text-sm">Sage Eldercare Solutions</div>
-          <div className="text-sm underline"><Link href="https://www.sageeldercare.com/">sageeldercare.com</Link></div>
-          <div className="text-sm">(650) 652-5642</div>
-          <div className="text-sm">cmigdal@sageeldercare.com</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">Sage Eldercare Solutions</div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5 underline"><Link href="https://www.sageeldercare.com/">sageeldercare.com</Link></div>
+          <div className="text-sm break-all px-2.5 break-all px-2.5">(650) 652-5642</div>
+          <div className="text-sm break-all px-2.5">cmigdal@sageeldercare.com</div>
         </div>
 
             <div className="mb-5 md:mb-0 h-full text-center shadow lg:shadow md:shadow-none rounded-lg rounded-lg w-[250px] lg:w-[250px] md:w-[170px] py-5 place-items-center place-self-center">
@@ -175,10 +179,10 @@ export default async function AboutUsPage() {
       </div></div>
           <div className="text-xl font-bold pt-2.5 text-[#272757]">Deonah Salari</div>
           <div className="text-md pb-2.5"></div>
-          <div className="text-sm">Anchor Health</div>
-          <div className="text-sm underline"><Link href="https://www.anchorhpc.com/">anchorhpc.com/</Link></div>
-          <div className="text-sm">(951) 255-0131</div>
-          <div className="text-sm">Dsharifi@anchorhpc.com</div>
+          <div className="text-sm break-all px-2.5">Anchor Health</div>
+          <div className="text-sm break-all px-2.5 underline"><Link href="https://www.anchorhpc.com/">anchorhpc.com/</Link></div>
+          <div className="text-sm break-all px-2.5">(951) 255-0131</div>
+          <div className="text-sm break-all px-2.5">Dsharifi@anchorhpc.com</div>
         </div>
         </div>
       </div>
